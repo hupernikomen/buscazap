b06f82a4833650b1a83f8daa376218b901295cde
Wilson Ramos <hupcontato@gmail.com>
2025-12-26 10:25:25 -0300

    commit

--- src/component/LogoHeader.js
+++ src/component/LogoHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Image } from 'react-native';
 
 const LOGO_URL =
-  'https://firebasestorage.googleapis.com/v0/b/appguiacomercial-e6109.appspot.com/o/buscazapthe3.png?alt=media&token=b46ea653-8e79-4b15-afd9-7d958e0096f0';
+  'https://firebasestorage.googleapis.com/v0/b/appguiacomercial-e6109.appspot.com/o/buscazaptop.png?alt=media&token=b46ea653-8e79-4b15-afd9-7d958e0096f0';
 
 export default function LogoHeader() {
   return (
